fb965ff7cc235698be08bcd21cc5f0e1cb1a22e4
suveng <suveng@163.com>
2019-02-28 09:55:22 +0800

    404 页面

--- src/main/java/my/suveng/springboot/common/base/BaseController.java
+++ src/main/java/my/suveng/springboot/common/base/BaseController.java
@@ -4,4 +4,5 @@ package my.suveng.springboot.common.base;
  * @author 苏文广 created at 2019/1/29
  */
 public class BaseController {
+
 }
--- src/main/java/my/suveng/springboot/common/exceptionHandler/ExceptionAdvice.java
+++ src/main/java/my/suveng/springboot/common/exceptionHandler/ExceptionAdvice.java
@@ -16,6 +16,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  **/
 @ControllerAdvice
 public class ExceptionAdvice {
+
+
     @ExceptionHandler({Exception.class})
     @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
     @ResponseBody
