67b513bffdd7bd67df957be7d416f8c7066f9154
suveng <suveng@163.com>
2019-05-31 13:46:54 +0800

    消除ck

--- src/main/java/my/suveng/server/controller/UserController.java
+++ src/main/java/my/suveng/server/controller/UserController.java
@@ -1,14 +1,10 @@
 package my.suveng.server.controller;
 
-import my.suveng.server.common.config.datasource.DataSourceKey;
-import my.suveng.server.common.config.datasource.DynamicDataSourceContextHolder;
 import my.suveng.server.common.response.Response;
 import my.suveng.server.common.response.ResponseBuilder;
 import my.suveng.server.common.response.ResponseEnums;
 import my.suveng.server.modules.user.entity.User;
-import my.suveng.server.modules.user.entity.UserCk;
 import my.suveng.server.modules.user.mapper.mysql.UserMapper;
-import my.suveng.server.modules.user.service.impl.UserCkService;
 import my.suveng.server.modules.user.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,20 +21,20 @@ import java.util.List;
 @RestController
 @RequestMapping("/user")
 public class UserController {
-    @Autowired
-    private UserCkService userCkService;
+    //@Autowired
+    //private UserCkService userCkService;
     @Autowired
     private UserMapper userMapper;
 
     @Autowired
     private UserServiceImpl userService;
-    @RequestMapping("/get")
-    public Response get(){
-        DynamicDataSourceContextHolder.set(DataSourceKey.ck);
-        int i = userCkService.selectTotal(null);
-        List<UserCk> userCks = userCkService.selectList();
-        return ResponseBuilder.build(ResponseEnums.SIMPLE_SUCCESS,userCks);
-    }
+    //@RequestMapping("/get")
+    //public Response get(){
+    //    DynamicDataSourceContextHolder.set(DataSourceKey.ck);
+    //    int i = userCkService.selectTotal(null);
+    //    List<UserCk> userCks = userCkService.selectList();
+    //    return ResponseBuilder.build(ResponseEnums.SIMPLE_SUCCESS,userCks);
+    //}
     @RequestMapping("/getUser")
     public Response getUser(){
         //DynamicDataSourceContextHolder.set(DataSourceKey.mysql);
--- src/main/java/my/suveng/server/modules/user/service/impl/UserCkService.java
+++ src/main/java/my/suveng/server/modules/user/service/impl/UserCkService.java
@@ -1,32 +1,32 @@
-package my.suveng.server.modules.user.service.impl;
-
-import my.suveng.server.modules.user.entity.UserCk;
-import my.suveng.server.modules.user.mapper.ck.UserCkMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-/**
- * description:
- * @author suwenguang@52tt.com
- * @date 2019/5/27
- * @version 1.0.0
- **/
-@Service
-public class UserCkService {
-    @Autowired
-    private UserCkMapper userCkMapper;
-
-    public int selectTotal(UserCk userCk){
-        return userCkMapper.selectTotal(userCk);
-    }
-
-    public List<UserCk> selectList() {
-        return userCkMapper.selectList();
-    }
-
-    //public int save(){
-    //
-    //}
-}
+//package my.suveng.server.modules.user.service.impl;
+//
+//import my.suveng.server.modules.user.entity.UserCk;
+//import my.suveng.server.modules.user.mapper.ck.UserCkMapper;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+///**
+// * description:
+// * @author suwenguang@52tt.com
+// * @date 2019/5/27
+// * @version 1.0.0
+// **/
+//@Service
+//public class UserCkService {
+//    @Autowired
+//    private UserCkMapper userCkMapper;
+//
+//    public int selectTotal(UserCk userCk){
+//        return userCkMapper.selectTotal(userCk);
+//    }
+//
+//    public List<UserCk> selectList() {
+//        return userCkMapper.selectList();
+//    }
+//
+//    //public int save(){
+//    //
+//    //}
+//}
